91d1649e060d613552cdcde1696fcd5dcb6c2320
Vishal Garg <vgarg@mphrx.com>
2021-04-15 01:00:11 +0530

    calc

---
+++ app.js
@@ -0,0 +1,29 @@
+const buttons = document.querySelectorAll('button');
+const screen = document.getElementById('screen');
+
+for (let btn of buttons) {
+    
+    btn.addEventListener('click', (e) => {
+        // console.log(e.target)
+        let buttonText = e.target.innerText;
+        
+        if (buttonText == 'x') {
+            screen.value += '*';
+        }
+        else if (buttonText == 'C') {
+            screen.value = "";
+        }
+        else if (buttonText == '=') {
+
+            try {
+                screen.value=eval(screen.value);
+            } catch (e) {
+                screen.value = "Invalid Operation";
+                console.log(e.message);
+            }   
+        }
+        else {
+            screen.value += buttonText;
+        }
+    }) 
+}
